9dd731233f0ace8748731cf6a3bbca3f3754725d
Ethermind <luis.capra@gmail.com>
2012-03-31 00:23:03 -0300

    email added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :message => "name can't be blank"
+  validates_length_of :name, :minimum => 3, :too_short => "name should have at least 3 characters"
+  validates_uniqueness_of :name, :message => "user already exists"
+  validates_presence_of :email, :message => "email can't be blank"
+  validates_length_of :email, :minimum => 6, :too_short => "email address too short"
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create, :message => "email address is invalid"
+  validates_uniqueness_of :email, :message => "email already exists"
   validates_presence_of :password, :message => "password can't be blank"
   validates_confirmation_of :password, :message => "password should match confirmation"
-  validates_length_of :name, :minimum => 3, :too_short => "name should have at least 3 characters"
   validates_length_of :password, :minimum => 3, :too_short => "password should have at least 3 characters"
-  validates_uniqueness_of :name, :message => "user already exists"
-  #validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end
