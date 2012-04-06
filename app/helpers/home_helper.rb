ed0cb7779ceb534d5b09c70026c20d58f341bfe9
Juan <jppampin@gmail.com>
2012-04-06 02:23:52 -0300

    Adding more cool styles. Changing buttons and links, adding green / black backgrounds

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,13 @@
 module HomeHelper
+  def is_user_logged
+    !@user.nil?
+  end
+  
+  def is_admin_logged            
+    is_user_logged && @user.admin?
+  end
+
+  def is_user_registered_and_match_available
+     !@match.closed && @user && !@player
+  end
 end
